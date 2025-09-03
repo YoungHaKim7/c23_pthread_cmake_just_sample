51835322075893ae3ebead2bb27b0863c138d5f5
GYoung <ytok1108@aol.com>
2025-09-04 01:51:15 +0900

    Leak Memory Check___Sanitizer

--- 02_Sanitizers_Debugging_Memory_Thread/02_LSan_LeakSanitizer/src/main.c
+++ 02_Sanitizers_Debugging_Memory_Thread/02_LSan_LeakSanitizer/src/main.c
@@ -1,23 +1,9 @@
-#include <stdio.h>
-#include "merge_sort.h"
+#include <stdlib.h>
+#include <string.h>
 
-void print_array(int arr[], size_t size) {
-    for (size_t i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
-int main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    size_t arr_size = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Given array is \n");
-    print_array(arr, arr_size);
-
-    merge_sort(arr, arr_size);
-
-    printf("\nSorted array is \n");
-    print_array(arr, arr_size);
+int main()
+{
+    char* leak = malloc(128);
+    strcpy(leak, "this memory is leaked!"); // ❌ never freed
     return 0;
 }
